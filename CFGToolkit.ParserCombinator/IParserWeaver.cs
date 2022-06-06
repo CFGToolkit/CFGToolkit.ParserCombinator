c2598c4cdc584eae78dea2cdd3705dd33aeb9f5b
Marcin Golebiowski <xormus@gmail.com>
2022-06-06 22:14:04 +0200

    Remove leftover

--- CFGToolkit.ParserCombinator/IParserWeaver.cs
+++
@@ -1,9 +0,0 @@
-﻿using CFGToolkit.ParserCombinator.Input;
-
-namespace CFGToolkit.ParserCombinator
-{
-    public interface IParserWeaver
-    {
-        IParser<TToken, TResult> Create<TToken, TResult>(IParser<TToken, TResult> parser, bool updateState = true) where TToken : IToken;
-    }
-}
